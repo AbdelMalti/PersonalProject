7870240c93bbb22766b1cf926da4cf561fd4f338
Abdeljelil Malti <abdeljelil.malti@outlook.com>
2017-01-08 01:19:53 -0500

    Ajout du file metadata

---
+++ CalculatorProjet/Calculator/src/view/CalcultorInterface.java
@@ -0,0 +1,10 @@
+package view;
+
+/**
+ * Created by Abdeljelil on 2017-01-06.
+ */
+public class CalcultorInterface {
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
